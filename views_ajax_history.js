aabc09a2b532fb993902ddda0394c04b1eb63114
Théodore Biadala <theodore@biadala.net>
2012-01-04 01:31:27 +0100

    almost there

--- views_ajax_history.js
+++ views_ajax_history.js
@@ -1,73 +1,116 @@
 (function ($) {
 
-  function bindPager ($view) {
-    $view.find('ul.pager > li > a, th.views-field a, .attachment .views-summary a')
-      .unbind('click')
-      .bind('click', function (e) {
-        e.preventDefault();
-
-        History.pushState(null, document.title, $(this).attr('href'));
-        console.log(window.location.href);
-        $(window).triggerHandler('statechange');
-        $(this).triggerHandler('statechange');
-      });
-  }
-
-  function bindForm ($view) {
-    $view.find('.form-submit')
-      .unbind('click')
-      .bind('click', function (e) {
-        e.preventDefault();
-        var queryString = $(this.form).formSerialize();
-        var settings = Drupal.settings;
-        var url = settings.basePath + settings.pathPrefix;
-        // if ? in ajax_path, clean urls are off and we need to create the Path
-        if (!/\?/.test(settings.views.ajax_path)) {
-          url += Drupal.Views.getPath(window.location.href) + '?' + queryString;
-        }
-        else {
-          url += queryString;
-        }
-        History.pushState(null, document.title, url);
-      });
-  }
-
-
-  Drupal.views.ajaxView.prototype.attachExposedFormAjax = function() {
-    var button = $('input[type=submit], input[type=image]', this.$exposed_form);
-    button = button[0];
-    // @TODO virer l'utilisation de queryString dans url AJAX
-    console.log(this.element_settings);
-    this.element_settings.event = 'statechange';
-    this.exposedFormAjax = new Drupal.ajax($(button).attr('id'), button, this.element_settings);
+  // need to keep this to check if there are extra parameters to views
+  // have to filter views var at the same time
+  var original = {
+    path: window.location.href,
+    query: window.location.search || ''
   };
 
   /**
-   * Attach the ajax behavior to each link.
+   * Keep the original beforeSubmit to use it later
    */
-  Drupal.views.ajaxView.prototype.attachPagerAjax = function() {
-    this.element_settings.event = 'statechange';
-    this.$view.find('ul.pager > li > a, th.views-field a, .attachment .views-summary a')
-      .each(jQuery.proxy(this.attachPagerLinkAjax, this));
-  };
+  var beforeSubmit = Drupal.ajax.prototype.beforeSubmit;
 
-  // @TODO restore state when hitting back/forward
-  History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
-    var State = History.getState(); // Note: We are using History.getState() instead of event.state
-    console.log(State.data, State.title, State.url);
-  });
+  /**
+   * Keep the original beforeSerialize to use it later
+   */
+  var beforeSerialize = Drupal.ajax.prototype.beforeSerialize;
 
-  Drupal.behaviors.viewsAjaxHistory = {
-    attach: function (context, settings) {
-      var ajaxView = Drupal.views.ajaxView;
+  /**
+   * Strip views values and duplicates from URL
+   *
+   * @param url
+   * @param viewArgs
+   *
+   * @return url string
+   */
+  var cleanURL = function (url, viewArgs) {
+    var url = url.split('?');
+    var args = Drupal.Views.parseQueryString(url[1]);
+    var query = [];
 
-      // takes care of form and pagers
-      $.each(Drupal.settings.views.ajaxViews, function(i, settings) {
-        var $view = $('.view-dom-id-' + settings.view_dom_id);
-          //.ajaxSend(function () {console.log(arguments);});//.ajaxSend(ajaxHandle);
-        bindPager($view);
-        bindForm($view);
-      });
+    for (var i in args) {
+      if (args.hasOwnProperty(i)) {
+        query.push(i + '=' + args[i]);
+      }
     }
+
+    return url[0] + (query.length ? '?' + query.join('&') : '');
   };
+
+  /**
+   * unbind statechange when adding, easier
+   */
+  var addState = function (options, url) {
+    $(window).unbind('statechange', loadView);
+    History.pushState(options, document.title, cleanURL(url, options.data));
+    $(window).bind('statechange', loadView);
+  };
+
+  /**
+   * Make an AJAX request to update the view when nagitating back and forth.
+   * @param e
+   */
+  var loadView = function () {
+    var state = History.getState();
+    var options = state.data;
+
+    var $dummy = $('<div class="ajaxHistoryDummy"/>').appendTo($('body'));
+    var settings = $.extend({
+      submit: options.data,
+      setClick: true,
+      event: 'click',
+      selector: '.view-dom-id-' + options.data.view_dom_id,
+      progress: { type: 'throbber' },
+      success: function () { $dummy.remove(); }
+    }, options);
+
+    new Drupal.ajax(false, $dummy[0], settings);
+    // trigger ajax call
+    $dummy.trigger('click');
+  };
+
+  /**
+   * Handle pager links
+   *
+   * @param $element
+   * @param options
+   */
+  Drupal.ajax.prototype.beforeSerialize = function ($element, options) {
+    // don't do anything for this one
+    console.log($element);
+    if ($($element).hasClass('ajaxHistoryDummy')) {return;}
+
+    var url = original.path;
+    options.url = Drupal.settings.views.ajax_path;
+
+    // it's a form, wait for beforeSubmit
+    if ($element.is(':not(form)')) {
+      if ($element.is('a')) {
+        addState(options, $element.attr('href'));
+      }
+    }
+    //beforeSerialize.apply(this, arguments);
+  };
+
+  /**
+   * Handle exposed form submissions
+   *
+   * @param form_values
+   * @param element
+   * @param options
+   */
+  Drupal.ajax.prototype.beforeSubmit = function (form_values, element, options) {
+    var url = original.path + (/\?/.test(original.path) ? '&' : '?') + element.formSerialize();
+
+    // copy selected values in history state
+    $.each(form_values, function () {
+      options.data[this.name] = this.value;
+    });
+
+    addState(options, url);
+    beforeSubmit.apply(this, arguments);
+  };
+
 }(jQuery));
