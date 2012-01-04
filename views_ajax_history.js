6688ea171fab2d12140c58b9c53f63e93b3c2b79
Théodore Biadala <theodore@biadala.net>
2012-01-04 03:35:10 +0100

    oh yeah

--- views_ajax_history.js
+++ views_ajax_history.js
@@ -18,6 +18,46 @@
   var beforeSerialize = Drupal.ajax.prototype.beforeSerialize;
 
   /**
+   * Allow multiple value fields from Drupal.Views.parseQueryString()
+   *
+   * @param query
+   */
+  var parseQueryString = function (query) {
+    var args = {};
+    var pos = query.indexOf('?');
+    if (pos != -1) {
+      query = query.substring(pos + 1);
+    }
+    var pairs = query.split('&');
+    var pair, key, value;
+    for(var i in pairs) {
+      if (typeof(pairs[i]) == 'string') {
+        pair = pairs[i].split('=');
+        // Ignore the 'q' path argument, if present.
+        if (pair[0] != 'q' && pair[1]) {
+          key = decodeURIComponent(pair[0].replace(/\+/g, ' '));
+          value = decodeURIComponent(pair[1].replace(/\+/g, ' '));
+          // same value exists and end with []
+          if (/\[\]$/.test(key)) {
+            // make it an array if it's not already
+            if (!(key in args)) {
+              args[key] = [value];
+            }
+            // don't duplicate values
+            else if (!$.inArray(value, args[key]) !== -1) {
+              args[key].push(value);
+            }
+          }
+          else {
+            args[key] = value;
+          }
+        }
+      }
+    }
+    return args;
+  };
+
+  /**
    * Strip views values and duplicates from URL
    *
    * @param url
@@ -26,21 +66,30 @@
    * @return url string
    */
   var cleanURL = function (url, viewArgs) {
+    var args = parseQueryString(url);
     var url = url.split('?');
-    var args = Drupal.Views.parseQueryString(url[1]);
     var query = [];
 
-    for (var i in args) {
-      if (args.hasOwnProperty(i)) {
-        query.push(i + '=' + args[i]);
+    $.each(args, function (name, value) {
+      // use values from viewArgs if they exists
+      if (name in viewArgs) {
+        value = viewArgs[name];
       }
-    }
+      if ($.isArray(value)) {
+        $.merge(query, $.map(value, function (sub) {
+          return name + '=' + sub;
+        }));
+      }
+      else {
+        query.push(name + '=' + value);
+      }
+    });
 
     return url[0] + (query.length ? '?' + query.join('&') : '');
   };
 
   /**
-   * unbind statechange when adding, easier
+   * unbind statechange when adding a new state, don't need to refresh.
    */
   var addState = function (options, url) {
     $(window).unbind('statechange', loadView);
@@ -56,18 +105,17 @@
     var state = History.getState();
     var options = state.data;
 
-    var $dummy = $('<div class="ajaxHistoryDummy"/>').appendTo($('body'));
+    var $dummy = $('<div class="ajaxHistoryDummy"/>');
     var settings = $.extend({
       submit: options.data,
       setClick: true,
       event: 'click',
       selector: '.view-dom-id-' + options.data.view_dom_id,
-      progress: { type: 'throbber' },
-      success: function () { $dummy.remove(); }
+      progress: { type: 'throbber' }
     }, options);
 
     new Drupal.ajax(false, $dummy[0], settings);
-    // trigger ajax call
+    // trigger ajax call the element will be remove by the ajax insert command
     $dummy.trigger('click');
   };
 
@@ -79,7 +127,6 @@
    */
   Drupal.ajax.prototype.beforeSerialize = function ($element, options) {
     // don't do anything for this one
-    console.log($element);
     if ($($element).hasClass('ajaxHistoryDummy')) {return;}
 
     var url = original.path;
@@ -91,7 +138,7 @@
         addState(options, $element.attr('href'));
       }
     }
-    //beforeSerialize.apply(this, arguments);
+    beforeSerialize.apply(this, arguments);
   };
 
   /**
@@ -106,7 +153,17 @@
 
     // copy selected values in history state
     $.each(form_values, function () {
-      options.data[this.name] = this.value;
+      // field name ending with [] is a multi value field
+      if (/\[\]$/.test(this.name)) {
+        if (!options.data[this.name]) {
+          options.data[this.name] = [];
+        }
+        options.data[this.name].push(this.value);
+      }
+      // regular field
+      else {
+        options.data[this.name] = this.value;
+      }
     });
 
     addState(options, url);
